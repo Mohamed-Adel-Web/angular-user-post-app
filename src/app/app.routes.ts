aefbbed75aa0ae17d283d486d7210c4f205ccb16
Mohamed-Adel <151798616+Mohamed-Adel-Web@users.noreply.github.com>
2024-09-15 19:33:22 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { PostComponent } from './components/post/post.component';
+
 
 export const routes: Routes = [
 ];
